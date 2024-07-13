a92c5af1fbb3e8405b2365b008ec9342ab1cec8b
Saif Ali <saifali87211@gmail.com>
2024-07-13 23:44:15 +0530

    day 1 variables and data types

---
+++ day1/index.js
@@ -0,0 +1,37 @@
+/** Activity1 : Variable declartaion **/
+var weight = 56;
+console.log("Weight: ", weight);
+
+let height = 145;
+console.log("Height: ", height);
+
+/** Activity 2: Constant Declartion **/
+const isSubscribed = true;
+console.log("You have subscribed chaiaurcode yt channe? : ", isSubscribed);
+
+/** Activity 3: Data Types **/
+let speed = 150;
+let carModel = "Mercedes";
+let isElectric = "false";
+let carEngineSpec = {
+  fule: "Petrol",
+  acceleration: 8.9,
+};
+let cars = [carModel, "BMW", "Ford"];
+
+console.log("typeof speed:", typeof speed);
+console.log("typeof carModel:", typeof carModel);
+console.log("typeof isElectrical:", typeof isElectric);
+console.log("typeof carEngineSpec:", typeof carEngineSpec);
+console.log("typeof cars:", typeof cars);
+
+/** Activity 4: Reassigning Variables **/
+let tax = 5;
+console.log("Initail Value: ", tax);
+tax = 18;
+console.log("New Value: ", tax);
+
+/** Activity 5: Understanding const **/
+const pi = 3.14;
+pi = 15;    //Assignment to constant error
+console.log("pi = ", pi);   
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Javascript Challenge
+
+This repo is all contain daily pratice and solution from [chaiaurcode](https://chaicode.com/) [30 days js challenge](https://courses.chaicode.com/learn/home/30-days-of-Javascript-challenge) by Hitesh Sir.
+
+All Credits goes to Hites Sir.
+
+1. Day 1 :- [Variables and Datatypes](./day1/index.js)
